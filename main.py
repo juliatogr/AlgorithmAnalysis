9c6ec52cb6397acf8562911c194828f6aced6a36
juliatogr <julia.tortosa01@estudiant.upf.edu>
2024-04-29 20:51:38 +0200

    extract pitches from different extractors

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 import essentia.standard as ess
-
-import mir_eval
 import numpy as np
-import time
-import mido
 
 spectrum = ess.Spectrum()
 w = ess.Windowing(type='hann')
 framesize = 2048
 hopsize = 128
+sample_rate = 44100
 
 
 def pitchyinprobabilities(frame):
@@ -34,44 +31,10 @@ def compute_pitches_no_frames(audio, extractor):
     return audio_pitches
 
 
-
-# Load MIDI file and extract pitch and timing information
-def load_midi(filename):
-    midi_data = mido.MidiFile(filename)
-    notes = []
-    for msg in midi_data:
-        if msg.type == 'note_on':
-            pitch = msg.note
-            time = msg.time
-            notes.append((pitch, time))
-    return notes
-
-# Align MIDI pitches with audio pitches based on timing information
-def align_pitches(audio_pitches, audio_times, midi_notes):
-    aligned_pitches = []
-    aligned_times = []
-    midi_times = np.cumsum([note[1] for note in midi_notes])
-    midi_pitches = [note[0] for note in midi_notes]
-    audio_time_index = 0
-    for midi_time, midi_pitch in zip(midi_times, midi_pitches):
-        while audio_time_index < len(audio_times) and audio_times[audio_time_index] < midi_time:
-            aligned_times.append(audio_times[audio_time_index])
-            aligned_pitches.append(audio_pitches[audio_time_index])
-            audio_time_index += 1
-        aligned_times.append(midi_time)
-        aligned_pitches.append(midi_pitch)
-    return aligned_pitches, aligned_times
-
-# def measure_time(func, *args):
-#     start_time = time.time()
-#     result = func(*args)
-#     elapsed_time = time.time() - start_time
-#     return result, elapsed_time
-
 def main():
 
     audio_filename = "datasets/recordings/C Scale 1.wav"
-    # midi_filename = "datasets/generated_scales/C Major/C Scale .mid"
+    midi_filename = "datasets/generated_scales/C Major/C Scale .mid"
 
     audio_loader = ess.MonoLoader(filename=audio_filename)
     audio = audio_loader()
@@ -125,16 +88,6 @@ def main():
     # print(type(pitches_yinprobabilities))  #list
     # print(f'    - pitches: {pitches_yinprobabilities}')
     print(f'    - length: {len(pitches_yinprobabilities)}')
-
-
-
-    # midi_notes = load_midi(midi_filename)
-    # aligned_pitches, aligned_times = align_pitches(audio_pitches, np.arange(len(audio_pitches)), midi_notes)
-
-
-    # accuracy = mir_eval.melody.evaluate(np.asarray(midi_notes)[:,0], np.asarray(midi_notes)[:,1], np.asarray(aligned_pitches), np.asarray(aligned_times))
-    # print(f" Melodia Accuracy: {accuracy}")
-
 
 
 if __name__ == "__main__":
