2cea27a8c704bfccacac5483581c07ffcb686538
juliatogr <julia.tortosa01@estudiant.upf.edu>
2024-06-13 22:41:06 +0200

    finish stats

--- stats_measure.py
+++ stats_measure.py
@@ -1,14 +1,13 @@
+import copy
+
 import essentia.standard as ess
 import sys
+
 sys.path.append('./datasets/Kaggle/traditional-flute-dataset/')
 import numpy as np
-import matplotlib.pyplot as plt
 import csv
 import load as ld
-import librosa as lr
 from mir_eval import melody as mel
-
-
 
 spectrum = ess.Spectrum()
 w = ess.Windowing(type='hann')
@@ -44,11 +43,20 @@ def compute_pitches_no_frames(audio, extractor):
 
 
 def main():
+    metrics = ['Voicing Recall', 'Voicing False Alarm', 'Raw Pitch Accuracy', 'Overall Accuracy']
+    header = copy.deepcopy(metrics)
+    header.insert(0, 'Audio Fragment')
+
+    melodia_data = [header]
+    yin_data = [header]
+    yinfft_data = [header]
+    hps_data = [header]
+
     fragments = ld.list_of_fragments('./datasets/Kaggle/traditional-flute-dataset/dataset.csv')
 
     for fragment in fragments:
 
-        # load files: audio, gt, score
+        # load files: audio, gt
         audio_file = './datasets/Kaggle/traditional-flute-dataset/audio/' + fragment + '.wav'
         audio, t, fs = ld.audio(audio_file)
 
@@ -56,6 +64,7 @@ def main():
         gt_onset, gt_note, gt_duration = ld.ground_truth(gt_file)
         gt_array, gt_t, gt_index = ld.to_array(gt_onset, gt_note, gt_duration, fs, hopsize)
 
+        # compute pitches
         audio_loader = ess.MonoLoader(filename=audio_file)
         audio = audio_loader()
 
@@ -78,13 +87,13 @@ def main():
         pitches_yinfft = np.array(pitches_yinfft)
         pitches_hps = np.array(pitches_hps)
 
+        # compute times for pitches
         times = np.arange(len(gt_array)) * hopsize / sample_rate
 
-        # Generate new x positions (indices) for array y
+        # Interpolate pitches to match gt
         old_indices = np.linspace(0, 1, len(pitches_melodia))
         new_indices = np.linspace(0, 1, len(gt_array))
 
-        # Interpolate
         pitches_melodia = np.interp(new_indices, old_indices, pitches_melodia)
         pitches_yin = np.interp(new_indices, old_indices, pitches_yin)
         pitches_yinfft = np.interp(new_indices, old_indices, pitches_yinfft)
@@ -93,44 +102,50 @@ def main():
         try:
             melodia_metrics = mel.evaluate(gt_t, gt_array, times, pitches_melodia)
             yin_metrics = mel.evaluate(gt_t, gt_array, times, pitches_yin)
-            yinffy_metrics = mel.evaluate(gt_t, gt_array, times, pitches_yinfft)
+            yinfft_metrics = mel.evaluate(gt_t, gt_array, times, pitches_yinfft)
             hps_metrics = mel.evaluate(gt_t, gt_array, times, pitches_hps)
 
-            pitch_methods = [
-                ('PitchMelodia', pitches_melodia, melodia_metrics),
-                ('PitchYin', pitches_yin, yin_metrics),
-                ('PitchYinFFT', pitches_yinfft, yinffy_metrics),
-                ('PitchHPS', pitches_hps, hps_metrics),
-                ('Ground-Truth', gt_array, None)
-            ]
+            melodia_data.append(
+                [fragment, format_number(melodia_metrics[metrics[0]]), format_number(melodia_metrics[metrics[1]]),
+                 format_number(melodia_metrics[metrics[2]]), format_number(melodia_metrics[metrics[3]])])
+            yin_data.append(
+                [fragment, format_number(yin_metrics[metrics[0]]), format_number(yin_metrics[metrics[1]]),
+                 format_number(yin_metrics[metrics[2]]), format_number(yin_metrics[metrics[3]])])
+            yinfft_data.append(
+                [fragment, format_number(yinfft_metrics[metrics[0]]), format_number(yinfft_metrics[metrics[1]]),
+                 format_number(yinfft_metrics[metrics[2]]), format_number(yinfft_metrics[metrics[3]])])
+            hps_data.append(
+                [fragment, format_number(hps_metrics[metrics[0]]), format_number(hps_metrics[metrics[1]]),
+                 format_number(hps_metrics[metrics[2]]), format_number(hps_metrics[metrics[3]])])
 
-            data = [['Pitch Method', 'Mean', 'Standard Deviation']]
-            for method, pitches, metrics in pitch_methods:
-                data.append([method, format_number(np.mean(pitches)), format_number(np.std(pitches))])
-
-            data.append([])
-
-            data.append(
-                ['Pitch Method', 'Voicing Recall', 'Voicing False Alarm', 'Raw Pitch Accuracy', 'Overall Accuracy'])
-            for method, _, metrics in pitch_methods[:-1]:  # Exclude Ground-Truth from this part
-                data.append([
-                    method,
-                    format_number(metrics['Voicing Recall']),
-                    format_number(metrics['Voicing False Alarm']),
-                    format_number(metrics['Raw Pitch Accuracy']),
-                    format_number(metrics['Overall Accuracy'])
-                ])
-            csv_file = f'./statistics/{fragment}.csv'
-            with open(csv_file, 'w', newline='') as file:
-                writer = csv.writer(file)
-                writer.writerows(data)
 
         except Exception as e:
             print(f'Error in {fragment}: {e}')
 
+        csv_file = f'./statistics/melodia_stats.csv'
+        with open(csv_file, 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerows(melodia_data)
+
+        csv_file = f'./statistics/yin_stats.csv'
+        with open(csv_file, 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerows(yin_data)
+
+        csv_file = f'./statistics/yinfft_stats.csv'
+        with open(csv_file, 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerows(yinfft_data)
+
+        csv_file = f'./statistics/hps_stats.csv'
+        with open(csv_file, 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerows(hps_data)
+
 
 def format_number(value):
     return f'{value:.3f}'.replace('.', ',')
+
 
 if __name__ == "__main__":
     main()
